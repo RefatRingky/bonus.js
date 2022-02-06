0190259ff672be294d6d375d4d8cf93cce8a82b7
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-06 14:19:26 +0600

    forth commit

--- revision.js
+++ revision.js
@@ -46,8 +46,18 @@
 
 
 // sum using recursion
- let sum = 0;
- for (let i = 5; i >= 1; i--) {
-     sum = sum + i;  
- }
- console.log(sum);
+//  let sum = 0;
+//  for (let i = 5; i >= 1; i--) {
+//      sum = sum + i;  
+//  }
+//  console.log(sum);
+
+
+function sum(i) {
+    console.log(i);
+    if (i==1) {
+        return 1;
+    }
+    return i + sum(--i);
+}
+console.log(sum(5));
