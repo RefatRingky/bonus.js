4d280821e96857e4d056504d14212e08916d1c5f
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-06 13:18:14 +0600

    third commit

--- revision.js
+++ revision.js
@@ -34,12 +34,20 @@
 
 // recursive function
 
-function consoleNumber(i) {
+// function consoleNumber(i) {
     
-    if (i > 5) {
-        return;
-    }
-    console.log(i);
-    consoleNumber(i+1);
-}
-consoleNumber(1);
+//     if (i > 5) {
+//         return;
+//     }
+//     console.log(i);
+//     consoleNumber(i+1);
+// }
+// consoleNumber(1);
+
+
+// sum using recursion
+ let sum = 0;
+ for (let i = 5; i >= 1; i--) {
+     sum = sum + i;  
+ }
+ console.log(sum);
